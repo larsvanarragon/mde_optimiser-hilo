7e54891038e7f1d64aa89f4b9a7ecf84714d6dd7
Lars van Arragon <larsvanarragon@ip-145-116-133-26.wlan-int.ru.nl>
2021-05-27 15:51:05 +0200

    First Commit

---
+++ problems/nrp/src/test/java/nl/ru/icis/mdeoptimiser/hilo/problems/nrp/MainTest.java
@@ -0,0 +1,12 @@
+package nl.ru.icis.mdeoptimiser.hilo.problems.nrp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue( true );
+  }
+}
