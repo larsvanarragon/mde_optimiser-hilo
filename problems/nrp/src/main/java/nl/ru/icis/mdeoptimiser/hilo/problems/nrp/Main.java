87f715b757a3b3fd7c0e94457bd7e9b6b84b6863
Lars van Arragon <lars@arragone.nl>
2021-05-27 16:04:48 +0200

    Added model files from NRP problem, loaded model using the XMI file.

--- problems/nrp/src/main/java/nl/ru/icis/mdeoptimiser/hilo/problems/nrp/Main.java
+++ problems/nrp/src/main/java/nl/ru/icis/mdeoptimiser/hilo/problems/nrp/Main.java
@@ -1,7 +1,16 @@
 package nl.ru.icis.mdeoptimiser.hilo.problems.nrp;
 
+import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
+
+import models.nrp.nextReleaseProblem.EcorePackage;
+
 public class Main {
   public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
+    HenshinResourceSet resourceSet = new HenshinResourceSet("src/main/resources/nl/ru/icis/mdeoptimiser/hilo/problems/nrp/models");
+
+    var metamodel = EcorePackage.eINSTANCE;
+    var model = resourceSet.getResource("nrp-model-25-cus-50-req-203-sa.xmi").getContents().get(0);
+    
+    System.out.println(model.toString());
   }
 }
